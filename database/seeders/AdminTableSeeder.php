aad621f355f86030370d19a3ed3da3bf2100de49
unknown <mustafa.salama2608@gmail.com>
2021-04-07 06:37:52 +0200

    first commit

---
+++ database/seeders/AdminTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    
+        $admin = \App\Models\Admin::create([
+            'name' => 'super_admin',
+            'email' => 'super_admin@app.com',
+            'password' => bcrypt('123456'),
+            'image'   => 'default.png',
+        ]);
+
+        $admin->attachRole('super_admin');
+ 
+    }
+}
